fe74620b0d84af64d3fe43973fb8ba6e73ee67cf
ercz16 <ericzhou001@gmail.com>
2021-05-16 16:41:15 -0400

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Irq9UEORRS0Uz1JrCtyKGkU6mhtSlksnbVWt1jcmgCCe9OUaDog5GKsvRi5Dm5n9djRDSWTgJyU8RTiqOsh4a6a0096O77tLA';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
