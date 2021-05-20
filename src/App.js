1cb96651e3f78533e9e1061fcb79714737e2c161
ercz16 <ericzhou001@gmail.com>
2021-05-20 10:50:10 -0400

    implemented stuff

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { 
+  auth,
+  createUserProfileDocument
+} from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -19,7 +25,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -31,9 +37,9 @@ class App extends React.Component {
               ...snapShot.data()
             });
           });
-      } else {
-        setCurrentUser(userAuth)
-      }
+      } 
+
+      setCurrentUser(userAuth);
     });
   }
 
@@ -67,7 +73,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser 
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -9,13 +9,15 @@ import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import './collections-overview.styles.scss';
 
 
-const CollectionsOverview = ({ collections }) => (
-    <div className='collections-overview'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-    </div>
-);
+const CollectionsOverview = ({ collections }) => {
+    return (
+        <div className='collections-overview'>
+            {collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))}
+        </div>
+    );
+};
 
 const mapStateToProps = createStructuredSelector({
     collections: selectCollectionsForPreview
